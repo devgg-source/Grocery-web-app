12639cb0eb3a75870874da7ea649735c51afd4ee
devgg-source <ggkarthikmessi007@gmail.com>
2021-11-11 08:13:25 +0530

    products section scripts added..

--- js/script.js
+++ js/script.js
@@ -37,3 +37,19 @@ window.onscroll = () => {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 };
+
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
