4e2f8764ff09a0f72f6166b4dc8dd801be22a76d
devgg-source <ggkarthikmessi007@gmail.com>
2021-11-11 15:02:05 +0530

    review section scripts added..

--- js/script.js
+++ js/script.js
@@ -53,3 +53,19 @@ var swiper = new Swiper(".product-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
